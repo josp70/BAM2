31475a2ab949265540b6c3006b186b35d827f081
Jorge Pérez <josp.jorge@gmail.com>
2017-01-06 02:22:50 +0100

    implement route forecast based on forecast package

--- app/controllers/BamTables.scala
+++ app/controllers/BamTables.scala
@@ -13,7 +13,7 @@ import play.api.Logger
   */
 class BamTables @Inject() (val currentEnv: play.api.Environment, val currentConf: play.api.Configuration) extends Controller {
 
-  var pathTolsh: String = "";
+  //var pathTolsh: String = "";
   var pathRscript: String = "";
 
   def getAppPath( name: String ): String = {
@@ -42,12 +42,13 @@ class BamTables @Inject() (val currentEnv: play.api.Environment, val currentConf
   }
 
   def init():Unit = {
-    pathTolsh = getAppPath("path.tolapp")
-    pathRscript = getAppPath("pathRscript")
+    //pathTolsh = getAppPath("path.tolapp")
+    pathRscript = getAppPath("path.Rscript")
     // make sure the data directory structure is created
     ensureDir("data")
     ensureDir("data/uploaded")
     ensureDir("data/modelled")
+    ensureDir("data/forecast")
     ensureDir("data/logs")
   }
 
@@ -55,83 +56,56 @@ class BamTables @Inject() (val currentEnv: play.api.Environment, val currentConf
 
   def getDirUploaded(): java.io.File = currentEnv.getFile("data/uploaded")
   def getDirModelled(): java.io.File = currentEnv.getFile("data/modelled")
+  def getDirForecast(): java.io.File = currentEnv.getFile("data/forecast")
   def getDirLogs(): java.io.File = currentEnv.getFile("data/logs")
   def getDirTolResources(): java.io.File = currentEnv.getFile("scripts/tol")
   def getDirRResources(): java.io.File = currentEnv.getFile("scripts/R")
 
   def getFileUploaded(name: String): java.io.File = new java.io.File(getDirUploaded, name)
   def getFileModelled(name: String): java.io.File = new java.io.File(getDirModelled, name)
+  def getFileForecast(name: String): java.io.File = new java.io.File(getDirForecast, name)
   def getFileLog(name: String): java.io.File = new java.io.File(getDirLogs, name)
   def getFileTol(name: String): java.io.File = new java.io.File(getDirTolResources, name)
   def getFileR(name: String): java.io.File = new java.io.File(getDirRResources, name)
 
   def getPathTolApp(): String = ""
 
-  def upload1 = Action(parse.multipartFormData) { request =>
-    request.body.file("dataframe").map { file =>
-      Ok(s"File uploaded: ${file.filename}")
-    }.getOrElse {
-      BadRequest("File is missing")
-    }
-  }
-
-  def upload = Action(parse.multipartFormData) { request =>
-    if (pathTolsh == "") {
-      InternalServerError("unknown or invalid path to tolsh")
+  def postForecast = Action(parse.multipartFormData) { request =>
+    if (pathRscript == "") {
+      InternalServerError("unknown or invalid path to Rscript")
     } else {
-      request.headers.get("lang")
-      request.body.file("dataframe").map { file =>
-        Logger.info(s"Received: ${file.filename}")
-        val id = java.util.UUID.randomUUID.toString
-        val inputName = id + ".data"
-        val outputName = id + ".model"
-        var fileLog = id + ".log"
-        val fileInput = getFileUploaded(inputName)
-        val fileOutput = getFileModelled(outputName)
-        file.ref.moveTo(fileInput)
-        Logger.info("dataframe copied to " + fileInput.getPath)
-        // invoke TOL to model the data
-        //http://www.scala-lang.org/api/rc2/scala/sys/process/ProcessBuilder.html
-        val cmd = Seq(pathTolsh,
-          """-cText ID_Data=\"""" + id + """\"""",
-          """-cText InputData=\"""" + fileInput.getAbsolutePath + """\"""",
-          """-cText OutputData=\"""" + fileOutput.getAbsolutePath + """\"""",
-          getFileTol("process_data.tol").getAbsolutePath) #> getFileLog(fileLog)
-        cmd.run
-        Ok(id)
-      }.getOrElse {
-        BadRequest("dataframe is missing")
+      val methodForecast = request.getQueryString("method").getOrElse("forecast")
+      val formatData = request.getQueryString("format").getOrElse("csv")
+      Logger.info("method = " + methodForecast)
+      if (formatData == "csv") {
+        request.body.file("dataframe").map { file =>
+          Logger.info(s"Received: ${file.filename}")
+          val id = java.util.UUID.randomUUID.toString
+          val inputName = id + ".data"
+          val outputName = id + ".fc"
+          var fileLog = id + ".log"
+          val fileInput = getFileUploaded(inputName)
+          val fileOutput = getFileForecast(outputName)
+          file.ref.moveTo(fileInput)
+          Logger.info("dataframe copied to " + fileInput.getPath)
+          // invoke Rscript to forecast the data
+          //http://www.scala-lang.org/api/rc2/scala/sys/process/ProcessBuilder.html
+          val cmd = Seq(pathRscript, "--vanilla", getFileR("process_data.r").getAbsolutePath,
+            id, fileInput.getAbsolutePath, fileOutput.getAbsolutePath, formatData, methodForecast) #> getFileLog(fileLog)
+          cmd.run
+          Ok(id)
+        }.getOrElse {
+          BadRequest("dataframe is missing")
+        }
+      } else {
+        BadRequest(s"format=${formatData} invalid. Valid formats are: csv")
       }
     }
   }
 
-  def upload0 = Action(parse.temporaryFile) { request =>
-    if (pathTolsh == "") {
-      InternalServerError("unknown or invalid path to tolsh")
-    } else {
-      val id = java.util.UUID.randomUUID.toString
-      val inputName = id + ".data"
-      val outputName = id + ".model"
-      var fileLog = id + ".log"
-      val fileInput = getFileUploaded(inputName)
-      val fileOutput = getFileModelled(outputName)
-      request.body.moveTo(fileInput)
-      // invoke TOL to model the data
-      //http://www.scala-lang.org/api/rc2/scala/sys/process/ProcessBuilder.html
-      val cmd = Seq(pathTolsh,
-        "-cText ID_Data=\"" + id + "\"",
-        "-cText InputData=\"" + fileInput.getAbsolutePath + "\"",
-        "-cText OutputData=\"" + fileOutput.getAbsolutePath + "\"",
-        getFileTol("process_data.tol").getAbsolutePath) #> getFileLog(fileLog)
-      cmd.run
-      Ok(id)
-    }
-
-  }
-
- def download(id: String) = Action {
-    val outputName = id + ".model"
-    val fileOutput = getFileModelled(outputName)
+ def getForecast(id: String) = Action {
+    val outputName = id + ".fc"
+    val fileOutput = getFileForecast(outputName)
     if (fileOutput.exists()) {
       // https://www.playframework.com/documentation/2.5.x/ScalaStream
       Ok.sendFile(
--- scripts/R/process_data.r
+++ scripts/R/process_data.r
@@ -2,18 +2,66 @@
 args = commandArgs(trailingOnly=TRUE)
 
 # test if there are 3 arguments: if not, return an error
-if (length(args)!=3) {
-  stop("Three arguments must be provided id input.data output.model", call.=FALSE)
+if (length(args)!=5) {
+  stop("five arguments must be provided: <id> <input.data> <output.model> format method", call.=FALSE)
 }
+
+library(forecast)
+library(tolBasis)
 
 ID_Data = args[1]
 InputData = args[2]
 OutputData = args[3]
+InputFormat = args[4]
+MethodForecast = args[5]
 
 cat("ID_Data =", ID_Data, "\n")
 cat("InputData =", InputData, "\n")
 cat("OutputData =", OutputData, "\n")
+cat("InputFormat =", InputFormat, "\n")
+cat("MethodForecast =", MethodForecast, "\n")
 
-st = file.copy(InputData, OutputData)
+read.bst = function( path ) {
+  z1 = read.zoo(path, format="y%Ym%md%d", header=TRUE, sep=";")
+  z1[,-ncol(z1)]
+}
+
+build.ts = function( z ) {
+  t = ts(z)
+  ts(t, frequency=findfrequency(t))
+}
+
+getDating = function(name) {
+  switch(name,
+    Yearly = Yearly,
+    Quarterly = Quarterly,
+    Monthly = Monthly,
+    Weekly = Weekly,
+    Daily = Daily)
+}
+
+Sread.bst = function(path) {
+  table = read.csv2(path)
+  dating = getDating(names(table)[1])
+  firstD = as.Date(table[1,1], format="y%Ym%md%d")
+  Serie(as.numeric(table[,2]), dating, firstD)
+}
+
+Swrite.bst = function(s, path) {
+  data = data.frame(Sdates(s), as.numeric(s))
+  names(data) = c(Sdating(s), "forecast")
+  write.table(data, path, row.names=FALSE, quote=FALSE, sep=";")
+}
+
+z = Sread.bst(InputData)
+y = as.ts(z)
+fc = forecast(y)
+lastD_obs = Slast(z)
+firstD_fc = Dsucc(lastD_obs, Sdating(z))
+z_fc = Serie(as.numeric(fc$mean), Sdating(z), firstD_fc)
+
+Swrite.bst(z_fc, OutputData)
+
+#st = file.copy(InputData, OutputData)
 
 cat("BYE\n")
